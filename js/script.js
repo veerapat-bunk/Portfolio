e8aa546da8b345a386917530864af0ce28ccddbc
1630902094 <veerapat.bunk@bumail.net>
2022-03-05 23:01:42 +0700

    typing

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// set variable typed  and link html and .js id=typing
+var typed = new Typed('.typing', {
+    strings: ['Veerapat Bunkwang', 'Arm'],                  // set array information to show in website
+    typeSpeed: 120,                                          // command set speed forward
+    backSpeed: 80,                                          // command set speed backward
+    loop: true                                              // set loop infinity (forward-backward)
+  });
+ 
