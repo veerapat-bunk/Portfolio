8c938551afca4a0fc26f4d36658e0e7ed83191c8
1630902094 <veerapat.bunk@bumail.net>
2022-03-06 00:26:25 +0700

    cursor mouse

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
-// set variable typed  and link html and .js id=typing
-var typed = new Typed('.typing', {
+// set variable typed 
+var typed = new Typed('.typing', {                          // id=typing
     strings: ['Veerapat Bunkwang', 'Arm'],                  // set array information to show in website
     typeSpeed: 120,                                          // command set speed forward
     backSpeed: 80,                                          // command set speed backward
     loop: true                                              // set loop infinity (forward-backward)
   });
- 
+  // cursor animation
+  const cursor = document.querySelector('.cursor');        // input on cursor (variable) .querySelector: return check selector if no matches are found null is returned
+
+  document.addEventListener('mousemove', e => {            // if mousemove go to function e  
+      cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")    // Sets the value of an attribute || mousemove top pageY-10 mousemove left  pageX-10
+  })
+
+  
