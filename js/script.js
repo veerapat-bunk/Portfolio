ec2486cc1065bb40c630603adfbaff93558f04ae
1630902094 <veerapat.bunk@bumail.net>
2022-03-06 11:49:57 +0700

    button send

--- js/script.js
+++ js/script.js
@@ -9,7 +9,13 @@ var typed = new Typed('.typing', {                          // id=typing
   const cursor = document.querySelector('.cursor');        // input on cursor (variable) .querySelector: return check selector if no matches are found null is returned
 
   document.addEventListener('mousemove', e => {            // if mousemove go to function e  
-      cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")    // Sets the value of an attribute || mousemove top pageY-10 mousemove left  pageX-10
+      cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;");    // Sets the value of an attribute || mousemove top pageY-10 mousemove left  pageX-10
   })
-
-  
+ // save information
+ function save(){
+    let btnClear = document.querySelector('button');
+    let inputs = document.querySelectorAll('input');
+    btnClear.addEventListener('click', () => {
+        inputs.forEach(input =>  input.value = '');
+    });
+ }
