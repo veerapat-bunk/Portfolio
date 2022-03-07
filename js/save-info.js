2647e7729f9de9042eed6b603f112b29e3f79fac
1630902094 <veerapat.bunk@bumail.net>
2022-03-07 13:20:15 +0700

    font-size button

--- js/save-info.js
+++ js/save-info.js
@@ -17,15 +17,15 @@
         console.log("------------------------------------------------");
     });
  }
-function changefontsize(type){
-    let ids = ['#am','#de'];
-    ids.forEach(id =>{
-        let el = document.querySelector(id);
-        let fontSize = window.getComputedStyle(el,null).getPropertyValue("font-size")
-        fontSize = parseFloat(fontSize);
-        if(type == "increase"){
+function changeFontSize(type){                                                               // function button single parameter 
+    let ids = ['#am','#de'];                                                                 // link id text array 
+    ids.forEach(id =>{                                                                       // for each array element 
+        let el = document.querySelector(id);                                                 // get element
+        let fontSize = window.getComputedStyle(el,null).getPropertyValue("font-size")        // select command css (font-size)     
+        fontSize = parseInt(fontSize);                                                       // convert input string to output integer
+        if(type == "increase"){                                                              // if type input increase => id.font-size + 5 px 
             el.style.fontSize = (fontSize + 5) + "px";
-        }else{
+        }else{                                                                               // if type input non-increase => id.font-size - 5 px 
             el.style.fontSize = (fontSize - 5) + "px";
         }
     });
