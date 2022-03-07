71828a1af44d2e4b7daaa6c915df8f2acb30b02c
1630902094 <veerapat.bunk@bumail.net>
2022-03-07 09:28:46 +0700

    prototype +- fontSize (profile)

--- js/save-info.js
+++ js/save-info.js
@@ -17,3 +17,16 @@
         console.log("------------------------------------------------");
     });
  }
+function changefontsize(type){
+    let ids = ['#am','#de'];
+    ids.forEach(id =>{
+        let el = document.querySelector(id);
+        let fontSize = window.getComputedStyle(el,null).getPropertyValue("font-size")
+        fontSize = parseFloat(fontSize);
+        if(type == "increase"){
+            el.style.fontSize = (fontSize + 5) + "px";
+        }else{
+            el.style.fontSize = (fontSize - 5) + "px";
+        }
+    });
+}
