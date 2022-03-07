d31e728cd67d55750139e6b6e80315be2001ce12
1630902094 <veerapat.bunk@bumail.net>
2022-03-07 15:00:25 +0700

    toggle icon mode (dark-light)

--- js/script.js
+++ js/script.js
@@ -11,3 +11,14 @@ var typed = new Typed('.typing', {                          // id=typing
   document.addEventListener('mousemove', e => {            // if mousemove go to function e  
       cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;");    // Sets the value of an attribute || mousemove top pageY-10 mousemove left  pageX-10
   })
+// set mode dark-light
+var icon = document.getElementById("switch-mode");                // link between class=icon and icon (variable) 
+icon.onclick = function(){                                 // if click to function 
+    // switch icon img toggle 
+    document.body.classList.toggle("dark");                
+    if(document.body.classList.contains("dark")){
+        icon.src = "/picture/sun.png";
+    }else{
+        icon.src = "/picture/moon.png";
+    }
+};
